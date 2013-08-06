48f75220ace44aab6b091bc9c4544371c2628786
unicornzero <rylee.k@gmail.com>
2013-08-06 12:36:34 -0600

    use inject to count matches

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,19 +11,7 @@ module Codebreaker
     end
 
     def guess(guess)
-      exact_match_count = 0
-      number_match_count = 0
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
-      end
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
-      end
-      @output.puts '+'*exact_match_count + '-'*number_match_count
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
     end
 
     def exact_match?(guess, index)
@@ -32,6 +20,19 @@ module Codebreaker
 
     def number_match?(guess, index)
       @secret.include?(guess[index]) && !exact_match?(guess, index)
+    end
+
+    def exact_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (exact_match?(guess, index) ? 1 : 0)
+      end
+    end
+
+    def number_match_count(guess)
+      number_match_count = 0
+      (0..3).inject(0) do |count, index|
+        count + (number_match?(guess, index) ? 1 : 0)
+      end
     end
   end
 end
