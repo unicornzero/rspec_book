cbce4b52d7d7654bbdf59cecfc5163daa767f695
unicornzero <rylee.k@gmail.com>
2013-08-09 16:17:41 -0600

    refactored spec

--- messages/spec/controllers/messages_controller_spec.rb
+++ messages/spec/controllers/messages_controller_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe MessagesController do
   describe "POST create" do
+    let(:message) { mock_model(Message).as_null_object }
+
+    before { Message.stub(:new).and_return(message) }
+
     it "creates a new message" do
-      message = mock_model(Message).as_null_object
       Message.should_receive(:new).with("text" => "a quick brown fox").and_return(message)
       post :create, :message => { "text" => "a quick brown fox" }
     end
     
     it "saves the message" do
-      message = mock_model(Message)
-      Message.stub(:new).and_return(message)
       message.should_receive(:save)
       post :create
     end
