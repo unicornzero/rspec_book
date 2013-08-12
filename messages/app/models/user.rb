d99234fb817d71c6a0c887440c4815f8cf2052a2
unicornzero <rylee.k@gmail.com>
2013-08-11 19:08:30 -0600

    tracks sender's sent messages

--- messages/app/models/user.rb
+++ messages/app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   has_many :received_messages, :class_name => Message.name, :foreign_key => "recipient_id"
+  has_many :sent_messages, :class_name => Message.name, :foreign_key => "sender_id"
 
   attr_accessible :login
 
   def send_message(message_attrs)
-    Message.create! message_attrs
+    sent_messages.create! message_attrs
   end
 
 end
--- messages/db/schema.rb
+++ messages/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130812004739) do
+ActiveRecord::Schema.define(:version => 20130812010510) do
 
   create_table "messages", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130812004739) do
     t.integer  "recipient_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "sender_id"
   end
 
   create_table "users", :force => true do |t|
