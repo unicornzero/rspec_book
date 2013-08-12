162abe985fd184fc2973df52ed266fb02e4e23bf
unicornzero <rylee.k@gmail.com>
2013-08-12 15:51:15 -0600

    complete

--- messages/spec/models/subscription_spec.rb
+++ messages/spec/models/subscription_spec.rb
@@ -1,5 +1,12 @@
-#require 'spec_helper'
-#
-#describe Subscription do
-#  pending "add some examples to (or delete) #{__FILE__}"
-#end
+require 'spec_helper'
+
+describe Subscription do
+  describe "#can_send_message?" do
+    context "when a user has not reached the subscription limit for the month" do
+      it "returns true"
+    end
+    context "when a user has reached the subscription limit for the month" do
+      it "returns false"
+    end
+  end
+end
