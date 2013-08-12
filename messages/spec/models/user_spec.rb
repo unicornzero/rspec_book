606536374845cd719268f27464b1eb19a1b04e07
unicornzero <rylee.k@gmail.com>
2013-08-11 19:12:00 -0600

    refactored test user creation

--- messages/spec/models/user_spec.rb
+++ messages/spec/models/user_spec.rb
@@ -3,40 +3,38 @@ require 'spec_helper'
 describe User do
 
   describe "#send_message" do
-
+    before(:each) do
+      @zach = User.create!
+      @david = User.create!
+    end
+    
     context "when the user is under their subscription limit" do
       it "sends a message to another user" do
-        zach = User.create!
-        david = User.create!
-        msg = zach.send_message(
+        msg = @zach.send_message(
           :title => "Book Update",
           :text => "Beta 11 includes great stuff!",
-          :recipient => david
+          :recipient => @david
           )
-        david.received_messages.should == [msg]
+        @david.received_messages.should == [msg]
       end
 
       it "creates a new message with the submitted attributes" do
-        zach = User.create!
-        david = User.create!
-        msg = zach.send_message(
+        msg = @zach.send_message(
           :title => "Book Update",
           :text => "Beta 11 includes great stuff!",
-          :recipient => david
+          :recipient => @david
           )
         msg.title.should == "Book Update"
         msg.text.should == "Beta 11 includes great stuff!"
       end
 
       it "adds the message to the sender's sent messages"do
-        zach = User.create!
-        david = User.create!
-        msg = zach.send_message(
+        msg = @zach.send_message(
           :title => "Book Update",
           :text => "Beta 11 includes great stuff!",
-          :recipient => david
+          :recipient => @david
           )
-        zach.sent_messages.should == [msg]
+        @zach.sent_messages.should == [msg]
       end
 
     end
