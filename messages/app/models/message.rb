fda797d5ac7a7eb1b02724dfb33d07399e59c378
unicornzero <rylee.k@gmail.com>
2013-08-11 18:46:21 -0600

    initial specs for Message model passing

--- messages/app/models/message.rb
+++ messages/app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
-  # attr_accessible :title, :body
+  belongs_to :recipient, :class_name => "User"
+
+  attr_accessible :recipient_id, :text, :title, :recipient
+
+  validates_presence_of :title, :text, :recipient
+
 end
--- messages/db/schema.rb
+++ messages/db/schema.rb
@@ -11,11 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130809214747) do
+ActiveRecord::Schema.define(:version => 20130811230420) do
 
   create_table "messages", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "title"
+    t.text     "text"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
